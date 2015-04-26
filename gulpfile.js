2505cc37ac14027bc6b74d79aa4bb63f769d6c91
Rish <rish@users.noreply.github.com>
2015-04-26 16:30:20 +0100

    Update gulp to watch scripts and html

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var bs = require('browser-sync').create();
+var reload = bs.reload;
 
 gulp.task('default', function() {
   bs.init({
@@ -10,5 +11,8 @@ gulp.task('default', function() {
       }
     }
   });
+
+  gulp.watch("app/**/*.html").on('change', reload);
+  gulp.watch("app/scripts/**/*.js").on('change', reload);
 });
 
